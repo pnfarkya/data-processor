e9eb1687e99afec81dfbf1d73d28fb32c8cf4c69
pnfarkya <prineshfakya@gmail.com>
2023-11-26 01:02:23 +0530

    Updates

--- src/Components/Common/Config.js
+++
@@ -1,6 +0,0 @@
-//import axios from "axios";
-
-export const RestController = null;
-export const BaseApi = "http://127.0.0.1:9001";
-export const AlphaNumericEx = /((^[0-9]+[a-z]+)|(^[a-z]+[0-9]+))+[0-9a-z]+$/i;
-export const AlphaEx = /^[a-zA-Z]+$/
